da5f9547ceab243a2e07be4ad5efae32e5cf7b0f
arjun narayan <arjun.narayan46@gmail.com>
2016-10-11 13:19:57 +0000

    config again

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+WTF_CSRF_TOKEN = True
+SECRET_KEY = 'b4bbdd1d71007193efcb3895ac08b5d32736832f7aaa18e6'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://epsilon:housemd@localhost/app_db' #sqlite:///'+os.path.join(basedir,'test.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir,'db_repositrory')
+
+OAUTH_CREDENTIALS = {
+	"google":{
+		"client_id":'542603804553-k8ivsqbsbk5ji24epr0mng8849j1732r.apps.googleusercontent.com',
+		"client_secret":'dVSTYwRgih_Vhedhv10ltz-p'
+	},
+	"facebook":{
+		"client_id":'blah',
+		"client_secret":"blah"
+	}
+}
